982b2de22ebbf2cfd54345bdde24a334c2c04aa6
Navleen <navleensandhu0211@gmail.com>
2023-02-01 16:13:02 -0800

    added css and the user can now view the previously entred messages

--- src/MessageBoard.tsx
+++ src/MessageBoard.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react"
+import { format } from 'date-fns'
 
 export const MessageBoard = () => {
-   const[userInput, setUserInput] = useState<string>('')
-   const[userMessagesArray, setUserMessagesArray] = useState<string[]>([])
+  const [userInput, setUserInput] = useState<string>('')
+  const [userMessagesArray, setUserMessagesArray] = useState<string[]>([])
 
-   const addUserMessage = (userInput: string) => {
-    setUserMessagesArray(prev => prev.concat(userInput))
-   }
+  const addUserMessage = (userInput: string) => {
+    if (userInput) {
+      setUserMessagesArray(prev => prev.concat(userInput))
+    }
+  }
 
-   
-   
   return (
     <>
       <div className="h-screen" >
@@ -17,18 +18,30 @@ export const MessageBoard = () => {
         <div className="flex h-full">
           <div className="bg-cyan-700 w-1/5"></div>
           <div className="flex flex-1 p-4 flex-col justify-end items-center gap-4">
-            {userMessagesArray.map((message,i ) => <div className="w-full border-b border-slate-200">
-              <div>{message}</div>
+            <div className="overflow-auto w-full">
+              {userMessagesArray.map((message, i) => <div key={i} className="w-full border-b border-slate-200 hover:bg-slate-100 p-2 rounded-md">
+                <div className="flex gap-4">
+                  <div>usericon</div>
+                  <div>username</div>
+                  <div>{format(new Date(), 'h:mm b')}</div>
+                </div>
+                <div>{message}</div>
               </div>)}
+            </div>
+
             <hr className="border-t border-slate-400 w-full"></hr>
-            <div className="w-full border border-slate-200 rounded-md">
-              <div className="w-full bg-slate-200 h-10 p-2"></div>
-              <input onChange={(e) => setUserInput(e.target.value)} className="p-4 w-full outline-0" type='text' placeholder='Enter message'>
-              </input>
-              <div className="w-full flex justify-end p-2 bg-slate-200 h-10">
-                <span onClick={() => addUserMessage(userInput)} className="material-symbols-rounded">
-                  send
-                </span>
+            <div className="w-full border border-slate-100 rounded-md">
+              <div className="w-full bg-slate-100 h-10 p-2 flex gap-2"></div>
+              <textarea onChange={(e) => setUserInput(e.target.value)} className="p-4 w-full outline-0 whitespace-pre-wrap" placeholder='Enter message'>
+              </textarea>
+              <div className="w-full flex items-center justify-end p-2 h-10">
+                <div onClick={() => addUserMessage(userInput)} className={`${userInput ? 'bg-cyan-700' : ''} flex justify-center items-center px-4 py-1 rounded-md hover:cursor-pointer`}>
+                  <span
+                    className="material-symbols-rounded text-slate-200" >
+                    send
+                  </span>
+                </div>
+
               </div>
             </div>
 
