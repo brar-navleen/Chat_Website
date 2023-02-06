6a44798e3ba6c6c4a64b806ccd7512e94b0d98a8
Navleen <navleensandhu0211@gmail.com>
2023-02-06 14:07:00 -0800

    on hitting enter inside input the user is directed to next input

--- src/signUp/EmailVerification.tsx
+++ src/signUp/EmailVerification.tsx
@@ -1,4 +1,13 @@
+import { useEffect, useRef } from "react";
+
 export const Emailverification = (prop: { userEmailAddress: string }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRefTwo = useRef<HTMLInputElement>(null);
+  const checkPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key == 'Enter'){
+      inputRefTwo.current?.focus();
+    }
+  }
 
   return (
     <>
@@ -9,15 +18,15 @@ export const Emailverification = (prop: { userEmailAddress: string }) => {
           expires shortly, so please enter it soon. </div>
         <div className="flex gap-4 items-center text-4xl">
           <div className="flex">
-            <input maxLength={1} className="p-4 h-32 w-32 border border-black text-center"></input>
-            <input maxLength={1} className="p-4 h-32 w-32 border-y border-black text-center"></input>
-            <input maxLength={1} className="p-4 h-32 w-32 border border-black text-center"></input>
+            <input maxLength={1} ref={inputRef} onKeyDown={(e) => checkPress(e)} className="p-4 h-32 w-32 border border-black text-center"></input>
+            <input maxLength={1} ref={inputRefTwo}  className="p-4 h-32 w-32 border-y border-black text-center"></input>
+            <input maxLength={1}  className="p-4 h-32 w-32 border border-black text-center"></input>
           </div>
           <div></div>
           <div className="flex">
-            <input maxLength={1} className="p-4 h-32 w-32 border border-black text-center"></input>
-            <input maxLength={1} className="p-4 h-32 w-32 border-y border-black text-center"></input>
-            <input maxLength={1} className="p-4 h-32 w-32 border border-black text-center"></input>
+            <input maxLength={1}  className="p-4 h-32 w-32 border border-black text-center"></input>
+            <input maxLength={1}  className="p-4 h-32 w-32 border-y border-black text-center"></input>
+            <input maxLength={1}  className="p-4 h-32 w-32 border border-black text-center"></input>
           </div>
         </div>
 
