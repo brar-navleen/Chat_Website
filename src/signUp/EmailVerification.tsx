32cb690e8169053405bfb1137c04263dbb78e977
Navleen <navleensandhu0211@gmail.com>
2023-02-07 14:08:22 -0800

    user can now go back and forth between code inputs

--- src/signUp/EmailVerification.tsx
+++ src/signUp/EmailVerification.tsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 
 export const Emailverification = (prop: { userEmailAddress: string }) => {
 
-  const [userEnteredCode, setUserEnteredCode] = useState('')
+  const [userEnteredCode, setUserEnteredCode] = useState<string[]>([])
   console.log(userEnteredCode)
 
 
@@ -17,10 +17,22 @@ export const Emailverification = (prop: { userEmailAddress: string }) => {
 
 
 
-  const checkPress = (e: React.ChangeEvent<HTMLInputElement>,i:number ) => {
-    if ('0123456789'.includes(e.target.value)) {
-      inputRefs[i].current?.focus()
-      setUserEnteredCode(prev => prev.concat(e.target.value))
+  const checkPress = (e: React.KeyboardEvent<HTMLInputElement>, currentInputIndex: number) => {
+    if ('0123456789'.includes(e.key)) {
+      inputRefs[currentInputIndex + 1].current?.focus()
+      setUserEnteredCode(prev => {
+        const newPrev= [...prev]
+        newPrev[currentInputIndex] = e.key
+        return newPrev
+      })
+    }
+
+    if (e.key === "ArrowLeft") {
+      inputRefs[currentInputIndex - 1].current?.focus()
+    }
+
+    if (e.key === "ArrowRight") {
+      inputRefs[currentInputIndex + 1].current?.focus()
     }
   }
 
@@ -33,15 +45,15 @@ export const Emailverification = (prop: { userEmailAddress: string }) => {
           expires shortly, so please enter it soon. </div>
         <div className="flex gap-4 items-center text-4xl">
           <div className="flex">
-            <input value={userEnteredCode[0] || ''} maxLength={1} ref={inputRefs[0]} onChange={(e) => checkPress(e, 1)} className="p-4 h-32 w-32 border border-black text-center"></input>
-            <input value={userEnteredCode[1] || ''} maxLength={1} ref={inputRefs[1]} onChange={(e) => checkPress(e, 2)} className="p-4 h-32 w-32 border-y border-black text-center"></input>
-            <input value={userEnteredCode[2] || ''} maxLength={1} ref={inputRefs[2]} onChange={(e) => checkPress(e, 3)} className="p-4 h-32 w-32 border border-black text-center"></input>
+            <input value={userEnteredCode[0] || ''} maxLength={1} ref={inputRefs[0]} onChange={() => { }} onKeyDown={(e) => checkPress(e, 0)} className="p-4 h-32 w-32 border border-black text-center"></input>
+            <input value={userEnteredCode[1] || ''} maxLength={1} ref={inputRefs[1]} onChange={() => { }} onKeyDown={(e) => checkPress(e, 1)} className="p-4 h-32 w-32 border-y border-black text-center"></input>
+            <input value={userEnteredCode[2] || ''} maxLength={1} ref={inputRefs[2]} onChange={() => { }} onKeyDown={(e) => checkPress(e, 2)} className="p-4 h-32 w-32 border border-black text-center"></input>
           </div>
           <div></div>
           <div className="flex">
-            <input value={userEnteredCode[3] || ''} maxLength={1} ref={inputRefs[3]} onChange={(e) => checkPress(e, 4)} className="p-4 h-32 w-32 border border-black text-center"></input>
-            <input value={userEnteredCode[4] || ''} maxLength={1} ref={inputRefs[4]} onChange={(e) => checkPress(e, 5)} className="p-4 h-32 w-32 border-y border-black text-center"></input>
-            <input value={userEnteredCode[5] || ''} maxLength={1} ref={inputRefs[5]} onChange={(e) => checkPress(e, 5)} className="p-4 h-32 w-32 border border-black text-center"></input>
+            <input value={userEnteredCode[3] || ''} maxLength={1} ref={inputRefs[3]} onChange={() => { }} onKeyDown={(e) => checkPress(e, 3)} className="p-4 h-32 w-32 border border-black text-center"></input>
+            <input value={userEnteredCode[4] || ''} maxLength={1} ref={inputRefs[4]} onChange={() => { }} onKeyDown={(e) => checkPress(e, 4)} className="p-4 h-32 w-32 border-y border-black text-center"></input>
+            <input value={userEnteredCode[5] || ''} maxLength={1} ref={inputRefs[5]} onChange={() => { }} onKeyDown={(e) => checkPress(e, 5)} className="p-4 h-32 w-32 border border-black text-center"></input>
           </div>
         </div>
       </div>
