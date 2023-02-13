f9cf14da77b1de86e97534bcc99dd360fb8afbae
Navleen <navleensandhu0211@gmail.com>
2023-02-13 12:02:03 -0800

    upon clicking on name user can view his/her status

--- src/loggedIn_User_Workspace/MessageBoard.tsx
+++ src/loggedIn_User_Workspace/MessageBoard.tsx
@@ -13,6 +13,7 @@ export const MessageBoard = () => {
   const [userMessagesArray, setUserMessagesArray] = useState<userEnteredMessageDetails[]>([])
   const [displayChannels, setDisplayChannels] = useState<boolean>(false)
   const [displayDirectMessages, setDisplayDirectMessages] = useState<boolean>(false)
+  const [userStatus, setUserStatus] = useState<boolean>(false)
 
   const mdParser = new MarkdownIt();
 
@@ -93,6 +94,15 @@ export const MessageBoard = () => {
     setDisplayDirectMessages(prev => !prev)
   }
 
+  useEffect(() => {
+    const closeMenu = () => {
+      setUserStatus(false)
+    }
+    document.body.addEventListener('click', closeMenu)
+
+    return () => document.body.removeEventListener('click', closeMenu)
+  }, [])
+
   return (
     <>
       {query.loading && <div className="h-screen flex flex-col justify-center items-center" >
@@ -105,7 +115,21 @@ export const MessageBoard = () => {
       </div>}
       {!query.loading && <div className="h-screen">
         <div className="bg-cyan-800 h-12 flex justify-end p-4 items-center text-white">
-          <div className="relative">
+          <div className="relative hover: cursor-pointer" onClick={(e) => {
+            setUserStatus(prev => !prev)
+            e.stopPropagation()
+          }
+          }>
+            {userStatus && <div className="absolute h-80 w-80 top-10 right-0 flex flex-col justify-between bg-slate-100 rounded-md text-black" onClick={(e) => e.stopPropagation()}>
+              <div className="border-b border-slate-400 pb-4">
+                <div>{query.result && query.result.displayName}</div>
+                <div className="flex items-center gap-1">
+                  <div className="rounded-full h-2.5 w-2.5 bg-emerald-400"></div>
+                  <div>Active</div>
+                </div>
+              </div >
+              <div className="border-t border-slate-400 pt-4">Sign out of Your Workspace</div>
+            </div>}
             <div className="bg-cyan-400 rounded-md px-4 py-0.5 font-bold">{query.result && query.result.displayName}</div>
             <div className="rounded-full border-4 border-cyan-800 h-5 w-5 bg-emerald-400 absolute -right-1.5 top-3.5"></div>
           </div>
