951cf9b1b1e4e09fe470ae5b4317745f7c35cb28
Navleen <navleensandhu0211@gmail.com>
2023-02-13 14:37:10 -0800

    started server on localhost:3000

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('hellow')
+import express from 'express'
+
+const app: express.Application = express()
+
+const port: number = 3000
+
+app.get('/', (_req, _res) => {
+  _res.send("TypeScript With Express");
+})
+
+app.listen(port, () => {
+  console.log(`TypeScript with Express
+       http://localhost:${port}/`)
+})
