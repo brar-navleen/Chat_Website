8fcad18a2680b0ec8179f273f19425b84fb73ea1
Navleen <navleensandhu0211@gmail.com>
2023-02-14 15:16:32 -0800

    integrated prisma ORM with express

---
+++ server/src/script.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+
+const usersWithPosts = await prisma.user.findMany({
+  include: {
+    posts: true,
+  },
+})
+console.log(usersWithPosts)
+console.dir(usersWithPosts, { depth: null })
+}
+
+
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
