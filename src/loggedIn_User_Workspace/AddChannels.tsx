11e2c07f823b9604501a84f55886459e8a9cf6c9
Navleen <navleensandhu0211@gmail.com>
2023-02-20 17:17:35 -0800

    made add channels text operational on click
     - user can create channels by clicking on add or add channels button

--- src/loggedIn_User_Workspace/AddChannels.tsx
+++ src/loggedIn_User_Workspace/AddChannels.tsx
@@ -11,7 +11,7 @@ export const AddChannels = () => {
         <span onClick={() => setShowModal(true)} className="material-symbols-rounded hover: cursor-pointer hover:bg-[#ffffff30] hover:backdrop-blur px-1.5 py-0.5 rounded-md">
           add
         </span>
-        <div className=" hover: cursor-pointer hover:bg-[#ffffff30] hover:backdrop-blur px-1.5 py-0.5 rounded-md">Add Channels</div>
+        <div onClick={() => setShowModal(true)} className=" hover: cursor-pointer hover:bg-[#ffffff30] hover:backdrop-blur px-1.5 py-0.5 rounded-md">Add Channels</div>
 
         {showModal && <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
           <div className="relative w-2/5 my-6 mx-auto max-w-3xl">
