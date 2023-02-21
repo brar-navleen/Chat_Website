dcf1e948687c874a4bf35a23802c7f0e12276e0b
Navleen <navleensandhu0211@gmail.com>
2023-02-21 13:08:32 -0800

    the add new channel modal closes on clicking outside

--- src/loggedIn_User_Workspace/AddChannels.tsx
+++ src/loggedIn_User_Workspace/AddChannels.tsx
@@ -1,20 +1,32 @@
-import { useState } from "react";
+import e from "express";
+import { useEffect, useState } from "react";
 
 export const AddChannels = () => {
 
   const [showModal, setShowModal] = useState(false);
 
+  useEffect(() => {
+    document.body.addEventListener('click', () => {setShowModal(false)})
+  },[])
+
   return (
     <>
-
       <div className="flex items-center gap-2">
-        <span onClick={() => setShowModal(true)} className="material-symbols-rounded hover: cursor-pointer hover:bg-[#ffffff30] hover:backdrop-blur px-1.5 py-0.5 rounded-md">
+        <span onClick={(e) => {
+          setShowModal(true)
+          e.stopPropagation()
+        }} 
+          className="material-symbols-rounded hover: cursor-pointer hover:bg-[#ffffff30] hover:backdrop-blur px-1.5 py-0.5 rounded-md">
           add
         </span>
-        <div onClick={() => setShowModal(true)} className=" hover: cursor-pointer hover:bg-[#ffffff30] hover:backdrop-blur px-1.5 py-0.5 rounded-md">Add Channels</div>
+        <div onClick={(e) => {
+          setShowModal(true)
+          e.stopPropagation()
+      }} 
+        className=" hover: cursor-pointer hover:bg-[#ffffff30] hover:backdrop-blur px-1.5 py-0.5 rounded-md">Add Channels</div>
 
         {showModal && <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-          <div className="relative w-2/5 my-6 mx-auto max-w-3xl">
+          <div onClick={(e) => e.stopPropagation() }  className="relative w-2/5 my-6 mx-auto max-w-3xl">
             <div className="rounded-lg shadow-lg relative flex flex-col w-full">
               <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
                 <div className="text-3xl font-semibold text-cyan-700">Create a new channel</div>
@@ -63,7 +75,6 @@ export const AddChannels = () => {
         </div>
         }
       </div>
-
     </>
   )
 }
